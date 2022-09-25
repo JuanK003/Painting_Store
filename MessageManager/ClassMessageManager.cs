eef0bcb6bcdeed2ff158a17aa96a3ba8222b669c
unrealJuanpa <juanpacloud@gmail.com>
2022-09-25 17:09:17 -0600

    Cambios revertidos a version estable

---
+++ MessageManager/ClassMessageManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageManager
+{
+    internal class ClassMessageManager
+    {
+        public ClassMessageManager()
+        {
+
+        }
+
+        public void Show(System.Windows.Forms.IWin32Window owner, string message)
+        {
+            if (message.ToLower().Contains("error"))
+            {
+                MessageBox.Show(owner, message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show(owner, message, "INFO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
